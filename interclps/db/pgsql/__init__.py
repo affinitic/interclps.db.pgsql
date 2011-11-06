05823c99f780e71b1f511620c1727995d8d498ef
alain <alain@2b13fa5e-b809-0410-b1a3-83a4c729f18f>
2011-11-06 21:48:19 +0000

    sans la variable pour le hook ##

--- interclps/db/pgsql/__init__.py
+++ interclps/db/pgsql/__init__.py
@@ -1,18 +1,18 @@
 import logging
-#from z3c.sqlalchemy import createSAWrapper
-#from zope.component import getUtility
-#from affinitic.pwmanager.interfaces import IPasswordManager
+from z3c.sqlalchemy import createSAWrapper
+from zope.component import getUtility
+from affinitic.pwmanager.interfaces import IPasswordManager
 
 LOGGER = 'interclps.db.pgsql'
 logging.getLogger(LOGGER).info('interclps.db.pgsql - Installing Product')
 
 
-#def initialize(context):
-#    pwManager = getUtility(IPasswordManager, 'pg')
-#    connString = 'postgres://%s@localhost/clpsbw' % pwManager.getLoginPassWithSeparator(':')
-#    wr = createSAWrapper(connString,
-#                        forZope=True,
-#                        echo=False,
-#                        engine_options = {'convert_unicode': True},
-#                        name='clpsbw',
-#                        model='clpsbwMappings')
+def initialize(context):
+    pwManager = getUtility(IPasswordManager, 'pg')
+    connString = 'postgres://%s@localhost/clpsbw' % pwManager.getLoginPassWithSeparator(':')
+    createSAWrapper(connString,
+                    forZope=True,
+                    echo=False,
+                    engine_options = {'convert_unicode': True},
+                    name='clpsbw',
+                    model='clpsbwMappings')
